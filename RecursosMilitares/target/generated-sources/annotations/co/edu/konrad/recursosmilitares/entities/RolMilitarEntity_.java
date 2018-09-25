f02961b2bb360aeb74d74d002921bac433437a67
Juan DIego Bernal <bernaljuandiego@hotmail.com>
2018-09-25 12:54:46 -0500

    crear editar roles militares

--- RecursosMilitares/target/generated-sources/annotations/co/edu/konrad/recursosmilitares/entities/RolMilitarEntity_.java
+++ RecursosMilitares/target/generated-sources/annotations/co/edu/konrad/recursosmilitares/entities/RolMilitarEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-25T00:08:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-25T10:03:32")
 @StaticMetamodel(RolMilitarEntity.class)
 public class RolMilitarEntity_ { 
 
