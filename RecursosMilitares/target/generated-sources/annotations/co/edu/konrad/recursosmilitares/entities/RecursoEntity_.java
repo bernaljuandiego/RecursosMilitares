5836ec4b105b33dcaba7ede69c50fb82bf71d401
Uriel Rodríguez <uriel.rodriguezv@konradlorenz.edu.co>
2018-10-17 18:30:52 -0500

    Logics de rama uriel
    Se crearon los logics de la rama uriel

--- RecursosMilitares/target/generated-sources/annotations/co/edu/konrad/recursosmilitares/entities/RecursoEntity_.java
+++ RecursosMilitares/target/generated-sources/annotations/co/edu/konrad/recursosmilitares/entities/RecursoEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-02T13:56:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-16T13:46:27")
 @StaticMetamodel(RecursoEntity.class)
 public class RecursoEntity_ { 
 
