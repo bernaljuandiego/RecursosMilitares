ea9082b0a55378f601646c18b7ce8aaa3ca3aed8
Uriel Rodríguez <uriel.rodriguezv@konradlorenz.edu.co>
2018-10-18 17:50:04 -0500

    Arreglo de api

--- RecursosMilitares/src/main/java/co/edu/konrad/recursosmilitares/resources/ApplicationConfig.java
+++ RecursosMilitares/src/main/java/co/edu/konrad/recursosmilitares/resources/ApplicationConfig.java
@@ -5,20 +5,21 @@
  */
 package co.edu.konrad.recursosmilitares.resources;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * Clase que registra el inventario de todos los recursos
- * @author Juan Diego Bernal Pedroza
+ * @author Juan Diego Bernal Pedroza y Uriel Rodríguez Vallarta
  */
 @ApplicationPath("/api")
 public class ApplicationConfig extends Application {
     
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        Set<Class<?>> resources = new HashSet<Class<?>>();
         addRestResourceClasses(resources);
         return resources;
     }
