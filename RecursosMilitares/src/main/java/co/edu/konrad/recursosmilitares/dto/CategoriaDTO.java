52f1bbee1ef70de1b2a476820f5ff0f881a04df8
Uriel Rodríguez <uriel.rodriguezv@konradlorenz.edu.co>
2018-10-17 19:18:48 -0500

    Dto's de rama uriel
    Se crearon los dto's correspondientes a mi rama uriel

--- RecursosMilitares/src/main/java/co/edu/konrad/recursosmilitares/dto/CategoriaDTO.java
+++ RecursosMilitares/src/main/java/co/edu/konrad/recursosmilitares/dto/CategoriaDTO.java
@@ -5,10 +5,84 @@
  */
 package co.edu.konrad.recursosmilitares.dto;
 
+import co.edu.konrad.recursosmilitares.entities.CategoriaEntity;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
+ * Clase que permite hacer la transferencia a Categoria
  * @author Uriel Rodríguez Vallarta
  */
 public class CategoriaDTO {
+   
+  /**
+   * Lista de atributos que hacen referencia al objeto Categoria
+   */    
+  private int id_categoria;
+  private String nom_categoria;
+  
+  /**
+   * Constructor por defecto
+   */  
+  
+    public CategoriaDTO() {
+    }
+ 
+    /**
+     * Mapeo de Entidad a Objeto
+     * @param CategoriaEntity 
+     */
+    public CategoriaDTO(CategoriaEntity CategoriaEntity) {
+        this.id_categoria = CategoriaEntity.getId_categoria();
+        this.nom_categoria = CategoriaEntity.getNom_categoria();
+    }
     
-}
+    /**
+     * Mapear de Objeto a Entidad
+     * @return 
+     */
+
+    public CategoriaEntity toEntity(){
+        CategoriaEntity entity = new CategoriaEntity(); 
+        entity.setId_categoria(this.id_categoria);
+        entity.setNom_categoria(this.nom_categoria);
+        
+        return entity;
+    }
+    
+    /**
+     * Conversor lista entidad a lista de objetos categoria
+     * @param categoriaEntitys
+     * @return 
+     */
+    
+    public static List<CategoriaDTO> tolistCategoria(List<CategoriaEntity> categoriaEntitys){
+        
+        List<CategoriaDTO> listaObjectoCategoria = new ArrayList<>();
+        for (int i = 0; i <categoriaEntitys.size(); i++) {
+            listaObjectoCategoria.add(new CategoriaDTO(categoriaEntitys.get(i)));
+        }
+        return listaObjectoCategoria;
+  
+    /**
+     * Metodos GET AND SET
+     * @return 
+     */
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public String getNom_categoria() {
+        return nom_categoria;
+    }
+
+    public void setNom_categoria(String nom_categoria) {
+        this.nom_categoria = nom_categoria;
+    }
+}//class
